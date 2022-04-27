1b7b658157fd7078a36d83e723d02bdbc56e4507
Karan Ravindra Fulare <fularekaran@yahoo.com>
2022-04-27 20:28:32 +0530

    added createpost form and upgraded react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Navbar, Home, CreatePost, PostDetail } from "./";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/post/:postId" component={PostDetail} />
-        <Route exact path="/create-post" component={CreatePost} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="post/:postId" element={<PostDetail />} />
+        <Route exact path="/create-post" element={<CreatePost />} />
+      </Routes>
     </div>
   );
 }
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreatePost() {
-  return <div>Posts</div>;
+  const [title, setTitle] = useState();
+  const [subtitle, setSubtitle] = useState();
+  const [content, setContent] = useState();
+
+  function handleSubmit(e) {
+    e.prevenDefault();
+    console.log("title", title);
+    console.log("title", subtitle);
+    console.log("title", content);
+  }
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+      <form>
+        <div className="form-field">
+          <lable>Title </lable>
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+
+        <div className="form-field">
+          <lable>Sub Title </lable>
+          <input
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-field">
+          <lable>Content</lable>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="create-post-btn">Create Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
