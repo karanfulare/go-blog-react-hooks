c6ecfb8111f6991b03510ca9c22b3c434442a414
Karan Ravindra Fulare <fularekaran@yahoo.com>
2022-04-27 14:15:54 +0530

    added routes , downgraded react-router-dom to v 5.3.0

---
+++ src/components/CreatePost.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreatePost(props) {
+  return <div>Posts</div>;
+}
+
+export default CreatePost;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import CreatePost from "./CreatePost";
+import PostDetail from "./PostDetail";
+import Home from "./Home";
+
+export { Navbar, CreatePost, PostDetail, Home };
