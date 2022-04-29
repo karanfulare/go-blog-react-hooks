6f08ac74bf8473b59f8772d75426763d95f8b5dc
Karan Ravindra Fulare <fularekaran@yahoo.com>
2022-04-29 10:34:23 +0530

    added the data in home component by a get req to firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    firestore
+      .collection("posts")
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setPosts(posts);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Blog </h1>
+      <div id="blog-by">Karan</div>
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subtitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
